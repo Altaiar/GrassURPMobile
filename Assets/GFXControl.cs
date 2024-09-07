963b50a14dc9d534a7442d1ed24aedeff9c886a2
Pouya <shateranip@gmail.com>
2024-09-07 14:16:21 +0330

    Init

---
+++ Assets/GFXControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+using TMPro;
+public class GFXControl : MonoBehaviour
+{
+    public UniversalRenderPipelineAsset myRenderer;
+    public TextMeshProUGUI Rendertext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        renderset();
+
+    }
+
+    public void renderset()
+    {
+        myRenderer.renderScale = GetComponent<Slider>().value;
+
+        Rendertext.text = myRenderer.renderScale.ToString().Substring(0, 4) ;
+    }
+}
