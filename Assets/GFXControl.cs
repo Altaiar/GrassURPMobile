5cf9768074b3dd9dd9f670558881d78d386857b4
Pouya <shateranip@gmail.com>
2024-09-08 18:01:40 +0330

    v1.1
    Enabled texture streaming
    better terrain Texture quality without sacrificing Performance

--- Assets/GFXControl.cs
+++ Assets/GFXControl.cs
@@ -11,7 +11,7 @@ public class GFXControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 120;
         renderset();
 
     }
